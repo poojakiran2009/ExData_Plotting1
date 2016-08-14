36bd77826f5378cbe73d403deaeaf6eff774c86a
poojakiran2009 <poojakiran2009@gmail.com>
2016-08-14 18:09:45 +0530

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -11,9 +11,18 @@ unlink (temp)
 tbl1 <- tbl [timestamp >= as.POSIXct ("2007-02-01", format = "%Y-%m-%d", origin="1970-1-1", tz="UTC") & timestamp <= as.POSIXct ("2007-02-02", format = "%Y-%m-%d", origin="1970-1-1", tz="UTC"),]
 
 # 
+tbl1 [,Datetime := timestamp]
 tbl1 [,weekday := wday (timestamp)]
-png ('plot2.png', height = 480, width = 480)
+png ('plot3.png', height = 480, width = 480)
+with(tbl1, {
+    plot(Sub_metering_1~Datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-plot (tbl1$timestamp, tbl1$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
+
 dev.off ()
 
