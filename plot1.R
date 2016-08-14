9ec3a252bf42ac985de3d17d09cf327028d0a27b
poojakiran2009 <poojakiran2009@gmail.com>
2016-08-14 17:52:06 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,3 @@
-dir.create ('tmp')
 temp <- tempfile ()
 download.file ("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", temp)
 unzip (temp, list=TRUE)
@@ -8,8 +7,11 @@ tbl [,timestamp := paste (Date, Time)]
 tbl [,timestamp :=as.POSIXct (timestamp, format = "%d/%m/%Y %H:%M:%S", origin="1970-1-1", tz="UTC")]
 unlink (temp)
 
+tbl1 <- tbl [timestamp >= as.POSIXct ("2007-02-01", format = "%Y-%m-%d", origin="1970-1-1", tz="UTC") & timestamp <= as.POSIXct ("2007-02-02", format = "%Y-%m-%d", origin="1970-1-1", tz="UTC"),]
+
 # 
 png ('plot1.png', height = 480, width = 480)
-hist (tbl$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col="red", border= "black")
+hist (tbl1$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col="red", border= "black")
 dev.off ()
+
 
